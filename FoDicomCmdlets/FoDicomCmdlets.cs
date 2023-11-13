68b9f6ccc7c73c64f3a296e4b0de06d2878e3e47
Katherine Masseau <katherine@fluxinc.ca>
2023-11-13 14:04:06 -0500

    add untracked

---
+++ FoDicomCmdlets/FoDicomCmdlets.cs
@@ -0,0 +1,16 @@
+﻿using Dicom;
+using Dicom.Network;
+using System;
+using System.Management.Automation;
+
+namespace FoDicomCmdlets
+{
+  [Cmdlet("Move", "StudyByStudyInstanceUID")]
+  public class MoveStudyByStudyInstanceUIDCmdlet : Cmdlet
+  {
+    protected override void ProcessRecord()
+    {
+      Console.WriteLine("MoveStudyByStudyInstanceUIDCmdlet happened.");
+    }
+  }
+}
