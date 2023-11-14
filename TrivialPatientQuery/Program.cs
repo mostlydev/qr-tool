1e73c0b391bd05b1218882934179dc71a9eb8ed6
Katherine Masseau <katherine@fluxinc.ca>
2023-11-13 19:33:09 -0500

    add TrivialPatientQuery, just a toy to test some queries.

---
+++ TrivialPatientQuery/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Dicom;
+using Dicom.Network;
+using DicomClient = Dicom.Network.Client.DicomClient;
+
+
+namespace TrivialPatientQuery
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.ReadLine(); // keep the window open.
+    }
+  }
+}
