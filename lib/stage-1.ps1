d99b768ae84feb09110d7f9cf13b09fa74dda8dc
Katherine Masseau <katherine@fluxinc.ca>
2023-11-15 13:23:14 -0500

    put stage1 in it's own file

---
+++ lib/stage-1.ps1
@@ -0,0 +1,60 @@
+######################################################################################################################################################
+# Do-Stage1: Examine files in $global:incomingStoredItemsDirPath and either accept them by moving them to $global:queuedStoredItemsDirPath or reject
+#            them.
+######################################################################################################################################################
+function Do-Stage1 {
+    Write-Indented " " # Just print a newline for output readability.
+    
+    $filesInIncomingStoredItemsDir = Get-ChildItem -Path $global:incomingStoredItemsDirPath -Filter *.dcm
+    
+    if ($filesInIncomingStoredItemsDir.Count -eq 0) {
+        Write-Indented "Stage #1: No DCM files found in incomingStoredItemsDir."
+    } else {
+        $counter = 0
+        
+        Write-Indented "Stage #1: Found $($filesInIncomingStoredItemsDir.Count) files in incomingStoredItems."
+
+        Indent
+        
+        foreach ($file in $filesInIncomingStoredItemsDir) {
+            $counter++
+
+            Write-Indented "Processing file #$counter/$($filesInIncomingStoredItemsDir.Count) '$(Trim-BasePath -Path $file.FullName)':"
+            
+            Indent
+            
+            $lastWriteTime = $file.LastWriteTime
+            $timeDiff      = (Get-Date) - $lastWriteTime
+
+            if (File-IsTooFresh -File $file) {
+                continue
+            }
+
+            $tags = Extract-StudyTags -File $file
+
+            WriteStudyTags-Indented -StudyTags $tags
+            
+            # The stage 1 hash is just name + DoB + study date, presumably the last is so that if the same patient comes in for
+            # another appointment in the future a new hash will be generated.
+            $studyHash      = Hash-String -HashInput "$($tags.PatientName)-$($tags.PatientBirthdate)-$($tags.StudyDate)"
+
+            Write-Indented " " # Just print a newline for output readability.
+
+            $hashedFileName = "$studyHash.dcm"
+            $foundFile      = Find-FileInDirectories -Filename $hashedFilename -Directories @($global:queuedStoredItemsDirPath, $global:processedStoredItemsDirPath)
+            
+            if ($foundFile -eq $null) {                
+                Write-Indented "Enqueuing $($file.Name) as $hashedFilename."
+                MaybeStripPixelDataAndThenMoveTo-Path -File $file -Destination (Join-Path -Path $global:queuedStoredItemsDirPath -ChildPath $hashedFileName)
+            } else {
+                Write-Indented "Item for hash $studyHash already exists as $(Trim-BasePath -Path $foundFile), rejecting."
+                Reject-File -File $file -RejectedDirPath $global:rejectedStoredItemsDirPath
+            }
+            
+            Outdent
+        } # foreach $file
+
+        Outdent
+    }
+}
+######################################################################################################################################################
--- qr-tool.ps1
+++ qr-tool.ps1
@@ -18,11 +18,12 @@ $global:foDicomExpectedDllPath = Join-Path -Path $PSScriptRoot -ChildPath "FoDic
 ######################################################################################################################################################
 # Include required function libs:
 ######################################################################################################################################################
-# These included files depend on each other and on globals defined here, so removing any of them is likely to cause problems: they are just being
-# used to keep the functions organized instead of having one huge file, not to make dependency management resilient.
+# These included files depend on each other and on globals defined here, so removing any of them or changing their order is likely to cause problems:
+# they are just being used to keep the functions organized instead of having one huge file, not to make dependency management resilient.
 #=====================================================================================================================================================
 . (Join-Path -Path $PSScriptRoot -ChildPath "lib\utility-funs.ps1")
 . (Join-Path -Path $PSScriptRoot -ChildPath "lib\dicom-funs.ps1")
+. (Join-Path -Path $PSScriptRoot -ChildPath "lib\stage-1.ps1")
 ######################################################################################################################################################
 
 
@@ -85,70 +86,13 @@ Require-DirectoryExists -DirectoryPath $global:processedStudyMovesDirPath  -Crea
 ######################################################################################################################################################
 
 
+
+
 ######################################################################################################################################################
 # Main:
 ######################################################################################################################################################
 do {
-    ##################################################################################################################################################
-    # Stage #1/3: Examine files in $global:incomingStoredItemsDirPath and either accept them by moving them to $global:queuedStoredItemsDirPath or
-    #            reject them.
-    ##################################################################################################################################################
-    Write-Indented " " # Just print a newline for output readability.
-    
-    $filesInIncomingStoredItemsDir = Get-ChildItem -Path $global:incomingStoredItemsDirPath -Filter *.dcm
-    
-    if ($filesInIncomingStoredItemsDir.Count -eq 0) {
-        Write-Indented "Stage #1: No DCM files found in incomingStoredItemsDir."
-    } else {
-        $counter = 0
-        
-        Write-Indented "Stage #1: Found $($filesInIncomingStoredItemsDir.Count) files in incomingStoredItems."
-
-        Indent
-        
-        foreach ($file in $filesInIncomingStoredItemsDir) {
-            $counter++
-
-            Write-Indented "Processing file #$counter/$($filesInIncomingStoredItemsDir.Count) '$(Trim-BasePath -Path $file.FullName)':"
-            
-            Indent
-            
-            $lastWriteTime = $file.LastWriteTime
-            $timeDiff      = (Get-Date) - $lastWriteTime
-
-            if (File-IsTooFresh -File $file) {
-                continue
-            }
-
-            $tags = Extract-StudyTags -File $file
-
-            WriteStudyTags-Indented -StudyTags $tags
-            
-            # The stage 1 hash is just name + DoB + study date, presumably the last is so that if the same patient comes in for
-            # another appointment in the future a new hash will be generated.
-            $studyHash      = Hash-String -HashInput "$($tags.PatientName)-$($tags.PatientBirthdate)-$($tags.StudyDate)"
-
-            Write-Indented " " # Just print a newline for output readability.
-
-            $hashedFileName = "$studyHash.dcm"
-            $foundFile      = Find-FileInDirectories -Filename $hashedFilename -Directories @($global:queuedStoredItemsDirPath, $global:processedStoredItemsDirPath)
-            
-            if ($foundFile -eq $null) {                
-                Write-Indented "Enqueuing $($file.Name) as $hashedFilename."
-                MaybeStripPixelDataAndThenMoveTo-Path -File $file -Destination (Join-Path -Path $global:queuedStoredItemsDirPath -ChildPath $hashedFileName)
-            } else {
-                Write-Indented "Item for hash $studyHash already exists as $(Trim-BasePath -Path $foundFile), rejecting."
-                Reject-File -File $file -RejectedDirPath $global:rejectedStoredItemsDirPath
-            }
-            
-            Outdent
-        } # foreach $file
-        ##############################################################################################################################################
-
-        Outdent
-    } # Stage #1/3
-    ##################################################################################################################################################
-
+    Do-Stage1
     
     ##################################################################################################################################################
     # Stage #2/3: Examine files in $global:queuedStoredItemsDirPath, create move request tickets in $global:queuedStudyMovesDirPath in for them and
