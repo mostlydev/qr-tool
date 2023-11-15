f90d15009d4763e0e19408b118f83bbc66f61c2e
Katherine Masseau <katherine@fluxinc.ca>
2023-11-15 13:56:34 -0500

    wip, perform the move.

--- lib/stage-3.ps1
+++ lib/stage-3.ps1
@@ -37,33 +37,34 @@ function Do-Stage3 {
                 Continue
             }
 
-            $lastResponseStatus = $null
-
-            if ($cMoveResponses -and $cMoveResponses.Count -gt 0) {
-                $lastResponseStatus = $cMoveResponses[-1].Status
-            } else {
-                Write-Indented "No responses received"
-            }
-            
-            if ($lastResponseStatus -eq [Dicom.Network.DicomStatus]::Success) {
-                Write-Indented "The last response appears to have been successful."
-            } elseif ($lastResponseStatus -eq $null) {
-                Write-Indented "The last response remains null. This is unusual."
-            } else {
-                Write-Indented "The last response appears not to have been successful. Status: $($lastResponseStatus)"
-            }
-
-            if ($lastResponseStatus -ne [Dicom.Network.DicomStatus]::Success) {
-                Write-Indented "Since move does not appear to have succeeded, $($file.FullName) will be deleted so as to allow future move attempts of the same hash."
-
+            if ($cMoveResponses -eq $null -or $cMoveResponses.Count -eq 0) {
+                Write-Indented "... no responses (or null responses) received. This is unusual. Removing queued study move $($file.FullName)."
                 Remove-Item -Path $file.FullName
-            }
-            else {
-                $processedStoredItemPath = Join-Path -Path $global:processedStoredItemsDirPath -ChildPath $file.Name
 
-                Write-Indented "Moving $($file.FullName) to $processedStoredItemPath"
-                Move-Item -Path $file.FullName -Destination $processedStoredItemPath
+                continue
             }
+
+            $cMoveStatus    = $cMoveResponses[-1]
+            $cMoveResponses = $cMoveResponses[0..($cMoveResponses.Count - 2)]
+
+            if ($cMoveStatus.Status -ne [Dicom.Network.DicomStatus]::Success) {
+                Write-Indented "... C-Move's final response status was $($cMoveStatus.Statua). Removing queued file $($file.FullName), user may re-store it to trigger a new attempt."
+                Remove-Item -Path $file.FullName
+
+                Continue
+            }
+
+            Write-Indented "... C-Move was successful."
+
+            # If the final response indicates sucess, we don't need need to examine the individual responses.
+            
+            $processedStudyMovePath = Join-Path -Path $global:processedStudyMovesDirPath -ChildPath $file.Name
+
+            Write-Indented " " # Just print a newline for output readability.
+            Write-Indented "Moving $(Trim-BasePath -Path $file.FullName) to $(Trim-BasePath -Path $processedStudyMovePath)... " -NoNewLine
+            Move-Item -Path $file.FullName -Destination $processedStudyMovePath
+            Write-Host " done."
+            
             
             Outdent
         } # foreach $file
