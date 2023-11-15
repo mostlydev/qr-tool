7e3973e7eb9ab2d65df0e9feecc2bf149a02bc86
Katherine Masseau <katherine@fluxinc.ca>
2023-11-15 15:52:09 -0500

    config.ps1

--- config.ps1
+++ config.ps1
@@ -13,7 +13,7 @@ $global:studyMoveFixedModality   = $null # "ZX"
 # ^ if $null, studies whose modality matches the stored item's are c-found (and subsequently c-moved), otherwise studies with this modality are found
 #   and moved.
 #=====================================================================================================================================================
-$global:sleepSeconds             = 3 # if greater than 0 script will loop, sleeping $global:sleepSeconds seconds each time.
+$global:sleepSeconds             = 0 # if greater than 0 script will loop, sleeping $global:sleepSeconds seconds each time.
 $global:mtimeThreshholdSeconds   = 3
 $global:largeFileThreshholdBytes = 50000
 $global:rejectByDeleting         = $true
