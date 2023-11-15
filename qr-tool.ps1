ec626c4c042cbfe59de9e0af965e4ae5161da868
Katherine Masseau <katherine@fluxinc.ca>
2023-11-15 13:08:33 -0500

    de-comment

--- qr-tool.ps1
+++ qr-tool.ps1
@@ -249,35 +249,6 @@ do {
             Move-Item -Path $file.FullName -Destination $processedStoredItemPath
             Write-Host " done."
             
-            # $moveResponses      = Move-StudyByStudyInstanceUID $tags.StudyInstanceUID
-            # $lastResponseStatus = $null
-
-            # if ($moveResponses -and $moveResponses.Count -gt 0) {
-            #     $lastResponseStatus = $moveResponses[-1].Status
-            # } else {
-            #     Write-Indented "No responses received"
-            # }
-            
-            # if ($lastResponseStatus -eq [Dicom.Network.DicomStatus]::Success) {
-            #     Write-Indented "The last response appears to have been successful."
-            # } elseif ($lastResponseStatus -eq $null) {
-            #     Write-Indented "The last response remains null. This is unusual."
-            # } else {
-            #     Write-Indented "The last response appears not to have been successful. Status: $($lastResponseStatus)"
-            # }
-
-            # if ($lastResponseStatus -ne [Dicom.Network.DicomStatus]::Success) {
-            #     Write-Indented "Since move does not appear to have succeeded, $($file.FullName) will be deleted so as to allow future move attempts of the same hash."
-
-            #     Remove-Item -Path $file.FullName
-            # }
-            # else {
-            #     $processedStoredItemPath = Join-Path -Path $global:processedStoredItemsDirPath -ChildPath $file.Name
-
-            #     Write-Indented "Moving $($file.FullName) to $processedStoredItemPath"
-            #     Move-Item -Path $file.FullName -Destination $processedStoredItemPath
-            # }
-            
             Outdent
         } # foreach $file
         ##############################################################################################################################################
